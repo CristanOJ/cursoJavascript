80840140f25b8a07ad91a41caab0294222746709
cristian <cristianjorge189@gmail.com>
2022-10-18 22:39:38 -0500

    var y let

--- 1.tiposDeDatos/ejercicios.js
+++ 1.tiposDeDatos/ejercicios.js
@@ -1,2 +1,6 @@
-console.log("jose") // string
+
 //ejecicio, hacer que la consola diga que tipo de dato es el que está
+let jose
+jose="string"
+
+console.log(jose)
--- 2.variables/ejercicios.js
+++ 2.variables/ejercicios.js
@@ -1,1 +1,4 @@
-//averiguar las diferencias entre let y var
+//Diferencias entre let y var
+//let te permite declarar variables limitando su alcance (scope) al bloque, declaración,
+// o expresión donde se está usando. a diferencia de la palabra clave var la cual define una variable global o local 
+//en una función sin importar el ámbito del bloque.
