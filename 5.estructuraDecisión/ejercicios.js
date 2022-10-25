a0b1abb3c49bbf05fa4a01785b10ce0fbf42fcbe
cristian <cristianjorge189@gmail.com>
2022-10-25 10:27:06 -0500

    25 de octubre 22

--- 5.estructuraDecisión/ejercicios.js
+++ 5.estructuraDecisión/ejercicios.js
@@ -5,3 +5,39 @@ if (Carro) {
 } else {
   console.log("No me gusta el carro");
 }
+
+Numeros=2
+switch(Numeros){
+case 1:
+console.log("uno")
+break
+case 2:
+console.log("dos")
+break
+case 3:
+console.log("tres")
+break
+case 4:
+console.log("cuatro")
+break
+default:
+console.log("Error number")
+}
+
+Letras="e"
+switch(Letras){
+case "a":
+console.log("Es vocal")
+break
+case "b":
+console.log("No es vocal")
+break
+case "c":
+console.log("No es vocal")
+break
+case "e":
+console.log("Es vocal")
+break
+default:
+console.log("Error number")
+}
