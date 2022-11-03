558f280fc6a3fca37ba9c3dc98adbc7d1d047ee4
cristian <cristianjorge189@gmail.com>
2022-11-03 10:42:25 -0500

    03 noviembre 2022

--- 5.estructuraDecisión/ejercicios.js
+++ 5.estructuraDecisión/ejercicios.js
@@ -36,3 +36,13 @@ break
 default:
 console.log("Error number")
 }
+//hacer un programa que me muestre el factorial de un numero
+5!=4*3*2*1
+5!=120
+
+7!=7*6*5*4*3*2*1
+//hacer un programa que me determine si un texto es palimdromo
+//<------Se lee de igual forma de cualquier lado "ADA"
+
+//hacer un programa que me muestre los n numeros de la serie fibonacci
+1,2,3,5,8,13 //para encontrar el numero siguiente, suma el anterior
--- 6.estructuraCiclo/ejercicios.js
+++ 6.estructuraCiclo/ejercicios.js
@@ -1,20 +1,40 @@
-//Existen 3 (concepto)
-//For
-//Sabemos en que momento va terminar
-for (let i=2;i<=10;i+=2){
+//con la estructura de ciclo for
+//mostrar los numeros pares entre 1 y 20
+for (let i=2;i<=20;i+=2){
     console.log(i)
 }
 
-//while
-//Permite crear un bucle infinito
-let eval=true
-let eval2="salir"
-while(eval==true){
-    console.log("hola")
-    if (eval2=="salir"){
-        console.log("adios")
-        eval=false
-    }
+for (let inicial=1;inicial<=20;inicial++){
+    if (inicial%2==0){   
+    console.log(inicial)
+}   else {
+    console.log(inicial)
+}
 }
 
-//do whilw
+let vocales="aeiou"
+console.log(vocales.length)
+console.log(vocales[0])
+for (let i=0;i<vocales.length;i++){
+    console.log(vocales[i])
+}
+
+let texto="hola como estas como te llamas"
+let contadore=0
+for (let i=0;i<texto.length;i++){
+    if (texto[i]=="a"){
+        contadore+=1
+    }
+}
+ console.log("tienes",contadore,"vocales a")
+//--------------------------------------------------------------------------------
+
+ let cantidad="eucalipto"
+ let contador=0
+ for (let i=0;i<cantidad.length;i++){
+    switch(cantidad[i]){
+        case "a": case "e": case "i": case "o": case "u":
+            contador+=1
+    }
+}
+console.log("tienes",contador,"vocales")
